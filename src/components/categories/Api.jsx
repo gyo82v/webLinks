a0d1fabfabece7a69e5111864579c994ac3f16a0
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-08-22 20:53:50 +0200

    Added one link

--- src/components/categories/Api.jsx
+++ src/components/categories/Api.jsx
@@ -7,6 +7,7 @@ export default function Api({open, toggle}){
             <Li href="https://openweathermap.org/current" name="current wheater">Wheater api</Li>
             <Li href="https://www.thecolorapi.com/docs#schemes" name="color scheme">Color scheme generator</Li>
             <Li href="https://www.deckofcardsapi.com/" name="deck of cards">game deck cards generator</Li>
+            <Li href="https://www.omdbapi.com/" name="open movie database">get info about movies and tv show</Li>
 
         </Section>
     )
