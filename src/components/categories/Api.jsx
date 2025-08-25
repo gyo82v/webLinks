f85912984fd7221e3ae6fba2cc43229471fbfb32
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-08-25 19:42:28 +0200

    Add 4 links

--- src/components/categories/Api.jsx
+++ src/components/categories/Api.jsx
@@ -5,9 +5,17 @@ export default function Api({open, toggle}){
     return (
         <Section open={open} toggle={toggle} title="Api">
             <Li href="https://openweathermap.org/current" name="current wheater">Wheater api</Li>
+            <Li href="https://openweathermap.org/weather-conditions" name="current wheater icons">Wheater api icons</Li>
             <Li href="https://www.thecolorapi.com/docs#schemes" name="color scheme">Color scheme generator</Li>
             <Li href="https://www.deckofcardsapi.com/" name="deck of cards">game deck cards generator</Li>
             <Li href="https://www.omdbapi.com/" name="open movie database">get info about movies and tv show</Li>
+            <Li href="https://www.coingecko.com/api/documentations/v3#/" name="crypto database">get info on crypto currency</Li>
+            <Li 
+              href="https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API/Using_the_Geolocation_API#getting_the_current_position"
+              name="geolocation api"
+            > 
+              get current location
+            </Li>
 
         </Section>
     )
--- src/components/categories/Css.jsx
+++ src/components/categories/Css.jsx
@@ -5,6 +5,12 @@ export default function Css({open, toggle}){
     return(
         <Section open={open} toggle={toggle} title="Css properties" >
             <Li href="https://developer.mozilla.org/en-US/docs/Web/CSS/overflow" name="overflow" >Overflow properties</Li>
+            <Li 
+              href="https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way" 
+              name="perfect background image "
+            >
+                Perfect full page bg image
+            </Li>
         </Section>
     )
 }
