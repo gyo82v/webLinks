3c9eeafd02c1ea2ede9c8699ebcfbf2104645ef2
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-09-29 20:24:28 +0200

    feat: add thress articles about web design

--- src/components/categories/Websites.jsx
+++ src/components/categories/Websites.jsx
@@ -8,6 +8,9 @@ export default function Websites({open, toggle}){
                view the best rated websites
             </Li>
             <Li href="https://chromewebstore.google.com/detail/momentum/laookkfknpbbblfpciffpaejjkokdgca?hl=en" name="momentum">Momentum dashboard fro browser</Li>
+            <Li href="https://anthonyhobday.com/sideprojects/saferules/" name="Visual desing rule">An article about desig rules</Li>
+            <Li href="https://webstyleguide.com/" name="Web style guide"> An article about web style</Li>
+            <Li href="https://www.adhamdannaway.com/blog/ui-design/ui-design-tips" name="ui design tips">An article about design tips</Li>
         </Section>
     )
 }
