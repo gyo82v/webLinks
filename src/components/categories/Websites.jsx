6c7a63bccb36961cb7e0beef41d5fca8aa5cc769
gyo82v <130491738+gyo82v@users.noreply.github.com>
2025-09-29 20:26:51 +0200

    feat: add two articles about web design

--- src/components/categories/Websites.jsx
+++ src/components/categories/Websites.jsx
@@ -11,6 +11,8 @@ export default function Websites({open, toggle}){
             <Li href="https://anthonyhobday.com/sideprojects/saferules/" name="Visual desing rule">An article about desig rules</Li>
             <Li href="https://webstyleguide.com/" name="Web style guide"> An article about web style</Li>
             <Li href="https://www.adhamdannaway.com/blog/ui-design/ui-design-tips" name="ui design tips">An article about design tips</Li>
+            <Li href="https://designingfortheweb.co.uk/" name="web design">An article about web design</Li>
+            <Li href="https://webstyleguide.com/" name="web style guide">An article about web style</Li>
         </Section>
     )
 }
